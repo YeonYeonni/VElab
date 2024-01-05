4fc42f343968a27e1e030fdaa91d2661f8aef76b
YeonYeonni <79257220+YeonYeonni@users.noreply.github.com>
2024-01-05 13:31:01 +0900

    Add files via upload

---
+++ C++/Lectures/01_Data_and_Logic/별찍기.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 별찍기
+	int N;
+	cin >> N;
+	for (int j = 0; j < N; j++)
+	{
+		for (int i = 0; i < N; i++)
+			cout << "*";
+		cout << endl;
+	}
+}
